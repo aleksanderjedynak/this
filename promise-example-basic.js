016a7d61d6087b608bb71c7cb94f706350ab0d33
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2018-10-01 01:41:58 +0200

    promise

---
+++ promise-example-basic.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let array =[{name: "Ala" }, {name: "Kate"}, {name: "Alex"}];
+
+let fetchArray = new Promise((resolve, reject) => {
+    let rand = Math.floor(Math.random()*2);
+
+    rand ? resolve(array) : reject({ status: 'error', message: "UPS!!!"});
+});
+
+fetchArray.then(resolved).catch(rejected);
+fetchArray.then(getAlex).then(resolved).catch(rejected);
+
+function getAlex(response){ return response[2]; }
+
+function resolved(response){ console.log(response) };
+
+function rejected(reason){ console.log(reason) };
+
