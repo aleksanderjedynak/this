34a69767eafd2891b2d950aa74a7a193a60ff1db
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2018-10-02 16:26:56 +0200

    onload and onerror method

--- promise/promise-example-xml-http-request-onload-version.js
+++ promise/promise-example-xml-http-request-onload-version.js
@@ -9,17 +9,24 @@ let users = (user) => {
 const promise = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", 'https://jsonplaceholder.typicode.com/users', true);
+
     xhr.onload = function(){
-        this.status === 200 ? 
-            resolve(JSON.parse(xhr.responseText)) :
-            reject("wrong promise!");
+        this.status === 200 && resolve(JSON.parse(xhr.responseText));
     };
+
+    xhr.onerror = function(){
+        reject(new Error("wrong promise!"));
+    };
+
     xhr.send();
 });
 
 promise
     .then((resolved) => {
-        console.log(['promise']);
+        console.log(['promise resolved']);
         users(resolved);
     })
-    .catch((err) => console.log(err));
+    .catch((rejected) => {
+        console.log(['promise rejected']);
+        console.log(rejected.message)
+    });
